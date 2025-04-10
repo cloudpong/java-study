e4db8515520811cfb361028b5ab6085f7148e214
cloudpong <rnldud16@gmail.com>
2025-04-10 21:56:09 +0900

    01~07 응용

---
+++ 01-basic/_Quize_01.java
@@ -0,0 +1,24 @@
+package chap_01;
+/*
+실행결과
+상암08번 버스
+약 3분 후 도착
+남은 거리 1.2km
+ */
+public class _Quize_01 {
+    public static void main(String[] args) {
+        String busName = "상암08";
+        System.out.println(busName + " 버스");
+        int time = 3;
+        System.out.println("약 " + time + "분 후 도착");
+        double distance = 1.2;
+        System.out.println("남은 거리 " + distance + "km");
+
+        /*문제 풀이
+        String busNo = "상암08";
+
+        int minute = 3;
+
+        double distance = 1.2;*/
+    }
+}
