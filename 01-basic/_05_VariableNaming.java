d754f465f2ec3fbf129f17b7b2ad6fa66367253e
cloudpong <rnldud16@gmail.com>
2025-04-10 21:52:25 +0900

    변수 이름 짓는 법

---
+++ 01-basic/_05_VariableNaming.java
@@ -0,0 +1,22 @@
+package chap_01;
+
+public class _05_VariableNaming {
+    public static void main(String[] args) {
+        // 입국 신고서 (여행)
+        String nantionality = "대한민국"; // 국적
+        String firstName = "현성"; // 이름
+        String lastName = "김"; // 성
+        String dateOfBirth = "2001-12-31"; // 생년월일
+        String residentialAddress = "무슨 호텔"; // 체류지
+        String purposeOfVisit = "관광"; //입국목적
+        String fligtNo = "KE657"; // 항공 편명
+        String _flightNo = "KE657"; // 밑줄 시작
+        String flight_no_2 = "KE657"; // 밑줄과 숫자 포함
+
+        int accompany = 2; //동반 가족 수
+        int lengthOfStay = 5; //치류기간
+
+        String item1 = "시계";
+        String item2 = "가방";
+    }
+}
